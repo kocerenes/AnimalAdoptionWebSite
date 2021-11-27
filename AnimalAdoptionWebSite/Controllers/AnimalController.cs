16fe214b264e3f09326fd55189c9ab3260033708
enes koçer <enes.kocer1@ogr.sakarya.edu.tr>
2021-11-27 18:16:34 +0300

    added are AnimalController and AddAnimal, Index,UpdateAnimal views.

---
+++ AnimalAdoptionWebSite/Controllers/AnimalController.cs
@@ -0,0 +1,79 @@
+﻿using AnimalAdoptionWebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalAdoptionWebSite.Controllers
+{
+    public class AnimalController : Controller
+    {
+        private readonly Context context;
+
+        public AnimalController(Context context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var listOfAnimals = context.Animals.ToList();
+            return View(listOfAnimals);
+        }
+
+        //Yeni hayvan eklemek için action
+        [HttpGet]
+        public IActionResult AddAnimal()
+        {
+            List<SelectListItem> valuesKind = (from k in context.Kinds.ToList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = k.TYPE_NAME,
+                                                       Value = k.KIND_ID.ToString()
+                                                   }).ToList();
+            ViewBag.fetchKindList = valuesKind;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddAnimal(Animal animal)
+        {
+            //var fetchKind = context.Kinds.Where(x => x.KIND_ID == animal.Kind.KIND_ID).FirstOrDefault();
+            //animal.Kind = fetchKind;
+            context.Animals.Add(animal);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //animal silme action ı
+        public IActionResult DeleteAnimal(int id)
+        {
+            var fetchAnimal = context.Animals.Where(x => x.ANIMAL_ID == id).FirstOrDefault();
+            context.Animals.Remove(fetchAnimal);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //id sini verdiğimizde, animal bulması için gereken action
+        public IActionResult GetAnimal(int id)
+        {
+            var fetchAnimal = context.Animals.Where(x => x.ANIMAL_ID == id).FirstOrDefault();
+            return View("UpdateAnimal", fetchAnimal);
+        }
+
+        //animal değerlerini güncellemek için action
+        public IActionResult UpdateAnimal(Animal animal)
+        {
+            var fetchAnimal = context.Animals.Where(x => x.ANIMAL_ID == animal.ANIMAL_ID).FirstOrDefault();
+            fetchAnimal.NAME = animal.NAME;
+            fetchAnimal.GENUS = animal.GENUS;
+            fetchAnimal.AGE = animal.AGE;
+            fetchAnimal.CITY = animal.CITY;
+            fetchAnimal.STATUS = animal.STATUS;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- AnimalAdoptionWebSite/Models/Animal.cs
+++ AnimalAdoptionWebSite/Models/Animal.cs
@@ -15,23 +15,29 @@ namespace AnimalAdoptionWebSite.Models
         //her hayvanın bir türü olur
         [ForeignKey("Kind")]
         public byte KIND_ID { get; set; }
+        [Display(Name = "Tür Adı")]
         public Kind Kind { get; set; }
 
         [Required(ErrorMessage = "Bu alanı doldurmak zorunlu!")]
         [MaxLength(25, ErrorMessage = "25 karakteri aşmayınız!")]
+        [Display(Name = "İsim")]
         public string NAME { get; set; }
 
         [MaxLength(25, ErrorMessage = "25 karakteri aşmayınız!")]
         [DisplayFormat(NullDisplayText = "Bilinmiyor")]
+        [Display(Name = "Cins")]
         public string GENUS { get; set; }
 
         [DisplayFormat(NullDisplayText = "Bilinmiyor")]
+        [Display(Name = "Yaş")]
         public byte AGE { get; set; }
 
         [Required(ErrorMessage = "Bu alanı doldurmak zorunlu!")]
         [MaxLength(20, ErrorMessage = "20 karakteri aşmayınız!")]
+        [Display(Name = "Şehir")]
         public string CITY { get; set; }
 
+        [Display(Name = "Mevcut Durumu")]
         public bool STATUS { get; set; } = true; //sahiplenilen canlıların durumu false olacak
 
         public ICollection<Adoption> Adoptions { get; set; }
