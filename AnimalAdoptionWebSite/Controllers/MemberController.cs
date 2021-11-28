7d4fabcbb7817abbe0738511607d606e202373c4
enes koçer <enes.kocer1@ogr.sakarya.edu.tr>
2021-11-28 22:52:32 +0300

    created MemberController. And created Index, Update views. Also update Member models.

---
+++ AnimalAdoptionWebSite/Controllers/MemberController.cs
@@ -0,0 +1,56 @@
+﻿using AnimalAdoptionWebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalAdoptionWebSite.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly Context context;
+
+        public MemberController(Context context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var listOfMember = context.Members.ToList();
+            return View(listOfMember);
+        }
+
+        //id sine göre üye bulma işlemi için action
+        public IActionResult GetMember(int id)
+        {
+            var fetchMember = context.Members.Where(x => x.MEMBER_ID == id).FirstOrDefault();
+            return View("UpdateMember", fetchMember);
+        }
+
+        //üye bilgilerini güncelleme işlemi için action
+        public IActionResult UpdateMember(Member member)
+        {
+            var fetchMember = context.Members.Where(x => x.MEMBER_ID == member.MEMBER_ID).FirstOrDefault();
+            fetchMember.NAMESURNAME = member.NAMESURNAME;
+            fetchMember.MAIL = member.MAIL;
+            fetchMember.PASSWORD = member.PASSWORD;
+            fetchMember.AGE = member.AGE;
+            fetchMember.PHONENUMBER = member.PHONENUMBER;
+            fetchMember.ADDRESS = member.ADDRESS;
+            fetchMember.ABOUT = member.ABOUT;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //member silme actionı
+        public IActionResult DeleteMember(int id)
+        {
+            var fetchMember = context.Members.Where(x => x.MEMBER_ID == id).FirstOrDefault();
+            context.Members.Remove(fetchMember);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- AnimalAdoptionWebSite/Models/Member.cs
+++ AnimalAdoptionWebSite/Models/Member.cs
@@ -14,30 +14,37 @@ namespace AnimalAdoptionWebSite.Models
 
         [Required(ErrorMessage = "Bu alanı doldurmak zorunlu!")]
         [MaxLength(50, ErrorMessage = "50 karakteri aşmayınız!")]
+        [Display(Name = "Ad Soyad")]
         public string NAMESURNAME { get; set; }
 
         [Required(ErrorMessage = "Bu alanı doldurmak zorunlu!")]
         [MaxLength(50, ErrorMessage = "50 karakteri aşmayınız!")]
+        [Display(Name = "Mail")]
         public string MAIL { get; set; }
 
         [Required(ErrorMessage = "Bu alanı doldurmak zorunlu!")]
         [MaxLength(25, ErrorMessage = "25 karakteri aşmayınız!")]
+        [Display(Name = "Şifre")]
         public string PASSWORD { get; set; }
 
         [Required(ErrorMessage = "Bu alanı doldurmak zorunlu!")]
         [Range(18, 50, ErrorMessage = "Üyelerin yaş aralığı 18-50 olmalıdır!")]
+        [Display(Name = "Yaş")]
         public byte AGE { get; set; }
 
         [MaxLength(11, ErrorMessage = "11 karakteri aşmayınız!")]
         [DisplayFormat(NullDisplayText = "---")]
+        [Display(Name = "Telefon No")]
         public string PHONENUMBER { get; set; }
 
         [Required(ErrorMessage = "Bu alanı doldurmak zorunlu!")]
         [MaxLength(120, ErrorMessage = "120 karakteri aşmayınız!")]
+        [Display(Name = "Adres")]
         public string ADDRESS { get; set; }
 
         [MaxLength(200, ErrorMessage = "200 karakteri aşmayınız!")]
         [DisplayFormat(NullDisplayText = "---")]
+        [Display(Name = "Hakkında")]
         public string ABOUT { get; set; }
 
         //üyeler birden fazla sahiplenme yapabilir
