4ab8de480d21e9a730256587b24dd9bb01096053
Enes Koçer <57600757+kocerenes@users.noreply.github.com>
2021-12-18 23:13:08 +0300

    added AdoptionList and update project design

---
+++ AnimalAdoptionWebSite/Controllers/AdoptionListController.cs
@@ -0,0 +1,29 @@
+﻿using AnimalAdoptionWebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalAdoptionWebSite.Controllers
+{
+    public class AdoptionListController : Controller
+    {
+        private readonly Context context;
+
+        public AdoptionListController(Context context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var ListOfAnimal = context.Adoptions
+                .Include(m => m.Member)
+                .Include(a => a.Animal)
+                .ToList();
+            return View(ListOfAnimal);
+        }
+    }
+}
